78d4a31e53e688357a3684fc41a4eebd67a47fbc
penguindoggy <penguinineteen@gmail.com>
2021-02-12 03:01:39 -0800

    first commit

---
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    public static int lives;
+    public static int money;
+    private int startLives = 20;
+    private int startMoney = 100;
+
+    void Start()
+    {
+        lives = startLives;
+        money = startMoney;
+    }
+}
---
+++ Assets/Scripts/Towers/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 5.0f;
+    public int damage = 20;
+    public float slowPercent = 0.3f;
+
+    public void LocateTarget (Transform _target) {
+        target = _target;
+    }
+
+    void Update() {
+        if (target == null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector2 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        // to make sure we can touch the target in this frame
+        // direction.magnitude -> the length of direction vector
+        if ( direction.magnitude <= distanceThisFrame) {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget() {
+        Destroy(gameObject);
+        target.GetComponent<Minion>().TakeDamage(damage);
+        target.GetComponent<Minion>().SlowDown(slowPercent);
+
+    }
+}
